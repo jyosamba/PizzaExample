89e9b80bfa4013b0e023b96575ee5092425326f8
Jaymeid <jamespawson65@gmail.com>
2023-10-23 16:50:33 +0100

    Plz work?

--- PizzaExample/src/main/java/Entities/AuthorEntity.java
+++ PizzaExample/src/main/java/Entities/AuthorEntity.java
@@ -1,5 +1,13 @@
 package Entities;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import javax.persistence.*;
 
 @Entity
--- PizzaExample/src/main/java/com/example/PizzaExample/PizzaExampleApplication.java
+++ PizzaExample/src/main/java/com/example/PizzaExample/PizzaExampleApplication.java
@@ -2,12 +2,26 @@ package com.example.PizzaExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class PizzaExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaExampleApplication.class, args);
+		System.out.println("Test");
+	}
+
+	@GetMapping("/")
+	public String index(){
+		return "Index";
+	}
+
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return "Hello "+ name;
 	}
 
 }
